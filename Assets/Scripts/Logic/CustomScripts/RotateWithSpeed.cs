3dfa760b0acd1c668b8c2b32c41857871ee803e1
Shevchenko Georg <3865georg@gmail.com>
2024-12-22 22:05:06 +0300

    changed player ship model

--- Assets/Scripts/Logic/CustomScripts/RotateWithSpeed.cs
+++ Assets/Scripts/Logic/CustomScripts/RotateWithSpeed.cs
@@ -13,23 +13,11 @@ public class RotateWithSpeed : MonoBehaviour
     private Quaternion _originalRotation = new Quaternion(0, 0, 0, 0);
 
 
-    private void FixedUpdate()
-    {
-        //MovementVector = ParentGameObject.GetComponent<CharacterController>().velocity;
-
-        //Vector3 rotationDIrection = Vector3.Cross(ParentGameObject.transform.up, new Vector3(MovementVector.x, 0, MovementVector.z));
-        //rotationDIrection *= -1;
-
-
-        //gameObject.transform.rotation = Quaternion.LookRotation(rotationDIrection, ParentGameObject.transform.right);
-    }
 
     private void Awake()
     {
-        //MovementVector = ParentGameObject.GetComponent<CharacterController>().velocity;
         ShipMove = ParentGameObject.GetComponent<ShipMove>();
         CharacterController = ParentGameObject.GetComponent<CharacterController>();
-
 
     }
 
@@ -39,16 +27,9 @@ public class RotateWithSpeed : MonoBehaviour
     {
 
         MovementVector = CharacterController.velocity;
-        //float angleY = -1 * Mathf.Asin(MovementVector.x) * RoationSpeed;
-        //float angle = -1 * Mathf.Atan2(MovementVector.z, MovementVector.y) * RoationSpeed;
-
-        //Quaternion rotation = Quaternion.Euler(angle, angleY, angle);
-
-        //transform.rotation = rotation;
 
         if(MovementVector != Vector3.zero)
         {
-            //transform.rotation = (Quaternion.LookRotation(new Vector3(0, 1, 0), MovementVector));
 
             transform.rotation = SmoothedRotation(transform.rotation, MovementVector);
             
